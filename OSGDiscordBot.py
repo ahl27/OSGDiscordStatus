df98f3a154f16d13938ace2095be317868c81cd1
ahl27 <ahl27@pitt.edu>
2022-07-08 11:23:35 -0400

    split out test file and main file to avoid janky testing methods

--- OSGDiscordBot.py
+++ OSGDiscordBot.py
@@ -247,17 +247,7 @@ def get_jobs_for_user(client, username):
   return(retval, totalstats)
 
 
-# testing block
-if __name__ == '!__main__':
-  print("Opening SSH connection...")
-  sshconnection = open_ssh_connection()
-  #gend_msg = get_all_jobs_status(sshconnection)
-  print(MSG_all_mobile_summaries(sshconnection, USERNAMES))
-  #print(MSG_most_recent_job(sshconnection, 'ahl'))
-  #print(MSG_all_user_jobs(sshconnection, 'ahl'))
-  #print(MSG_all_user_summaries(sshconnection, ['ahl']))
-
-# execution block, should split this up someday
+# execution block
 if __name__ == '__main__':
   #print("Opening SSH connection...")
   #sshconnection = open_ssh_connection()
---
+++ testBotOutput.py
@@ -0,0 +1,11 @@
+from OSGDiscordBot import *
+
+# testing block
+if __name__ == '__main__':
+  print("Opening SSH connection...")
+  sshconnection = open_ssh_connection()
+  #gend_msg = get_all_jobs_status(sshconnection)
+  print(MSG_all_mobile_summaries(sshconnection, USERNAMES))
+  #print(MSG_most_recent_job(sshconnection, 'ahl'))
+  #print(MSG_all_user_jobs(sshconnection, 'ahl'))
+  #print(MSG_all_user_summaries(sshconnection, ['ahl']))
